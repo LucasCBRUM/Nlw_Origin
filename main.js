4c94b2396e8dcc1a65553d4ebbc9e7a0cef9b447
Lucas Brum <79069638+LucasCBRUM@users.noreply.github.com>
2022-08-23 22:48:02 -0300

    i fixed the css problems

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-
+var y;
 function onScroll(){
     if (scrollY > 0){
         document.getElementById('navigation').classList.add('scroll');
     }else{
         document.getElementById('navigation').classList .remove('scroll');
     }
-    //console.log(scrollY);
 }
 
 function openMenu(){
     document.body.classList.add('menu-expanded');
+    y = window.scrollY;
+    window.scrollTo( 0, 0 );
 }
 
 function closeMenu(){
     document.body.classList.remove('menu-expanded');
+    window.scrollTo(0, y)
 }
