9c55bc73a6c76d3345c49fc1183b92dda4c44c94
Lucas Brum <79069638+LucasCBRUM@users.noreply.github.com>
2022-08-27 15:59:36 -0300

    adjust animation expanded-menu

--- main.js
+++ main.js
@@ -10,28 +10,28 @@ function onScroll(){
 function openMenu(){
     document.body.classList.add('menu-expanded');
     y = window.scrollY;
-    window.scrollTo( 0, 0 );
 }
 
 function closeMenu(a){
     document.body.classList.remove('menu-expanded');
     if(a = true){
         window.scrollTo(0, 0);
-        document.querySelector('#popUp').classList.add()
     }else{
         window.scrollTo(0, y);  
     }  
 }
 
-function onOpen(){
-    window.scroll(0, 0);
-}
 
-//criando um objeto
 
 
 ScrollReveal({
     origin: 'top',
     distance: '30px',
     duration: 700,
-}).reveal('#home, #home img, #home .stats');
+}).reveal(`
+#home,
+#home img,
+#home .stats,
+#services,
+#services header,
+#services .card`);
